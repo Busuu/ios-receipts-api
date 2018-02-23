9344e72d7537b18a15052f8385c8f7c57b344a92
arl91 <arubioleon91@gmail.com>
2018-02-23 11:55:12 +0000

    Stop to use purchase_date_ms to get the last receipt and use original_purchase_date_ms. We saw that we need to use the original_purchase_date_ms when the user is in a subscription with a free trial period

--- tests/ReceiptServiceTest.php
+++ tests/ReceiptServiceTest.php
@@ -28,6 +28,9 @@ class ReceiptServiceTest extends \PHPUnit_Framework_TestCase
         $this->receiptService = new ReceiptService($this->appleClient, $this->validatorService, ReceiptService::PRODUCTION_ENVIRONMENT);
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testGetLastReceiptSuccess()
     {
         $helper = new AppleHelper();
@@ -53,10 +56,13 @@ class ReceiptServiceTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(AppStoreReceipt::class, $storeReceipt);
         $this->assertEquals($storeReceiptData['receipt']['in_app'][0]['original_transaction_id'], $storeReceipt->getOriginalTransactionId());
         $this->assertEquals($storeReceiptData['receipt']['in_app'][0]['expires_date_ms'], $storeReceipt->getExpiresDateMs());
-        $this->assertEquals($storeReceiptData['receipt']['in_app'][0]['purchase_date_ms'], $storeReceipt->getPurchaseDateMs());
+        $this->assertEquals($storeReceiptData['receipt']['in_app'][0]['original_purchase_date_ms'], $storeReceipt->getOriginalPurchaseDateMs());
         $this->assertEquals($storeReceiptData['receipt']['in_app'][0]['product_id'], $storeReceipt->getProductId());
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testGetLastReceiptNoPurchase()
     {
         $helper = new AppleHelper();
